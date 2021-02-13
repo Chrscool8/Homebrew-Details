3e376af70e1581621d7cbec19ffd8f342c43a4ab
Chris Bradel <ChrisBradel14@gmail.com>
2021-02-13 00:17:05 -0500

    finalize(ish) changelogs

--- src/utils/panels.cpp
+++ src/utils/panels.cpp
@@ -269,7 +269,7 @@ void show_settings_panel()
             appView->sidebar->setWidth(1920);
             appView->setWidth(1920);
             appView->setHeight(400);
-            appView->sidebar->addView(new brls::Label(brls::LabelStyle::REGULAR, "\nUsing your favorite nsp installer, install the forwarder that is currently in:\n\n" + get_config_path() + "forwarder/HomebrewDetails_MultiForwarder.nsp\n\nIt will launch this application from any of the following locations:\n- sdmc:/switch/homebrew_details.nro\n- sdmc:/switch/homebrew_details/homebrew_details.nro\n- sdmc:/switch/homebrew-details/homebrew_details.nro", true));
+            appView->sidebar->addView(new brls::Label(brls::LabelStyle::REGULAR, "\nUsing your favorite nsp installer, install the forwarder that is currently in:\n\n" + get_config_path() + "forwarder/HomebrewDetailsForwarder_v2.nsp\n\nIt will launch this application from any of the following locations:\n- sdmc:/switch/homebrew_details.nro\n- sdmc:/switch/homebrew_details/homebrew_details.nro\n- sdmc:/switch/homebrew-details/homebrew_details.nro\n- sdmc:/switch/homebrew_details_alternate.nro", true));
             appView->setIcon(get_resource_path() + "arrows.png");
             brls::PopupFrame::open("How to Install to Home Menu", appView, "", "");
         });
@@ -336,8 +336,6 @@ void show_first_time_panel()
     brls::TabFrame* appView = new brls::TabFrame();
     appView->sidebar->setWidth(385);
 
-    //appView->addTab("Welcome", new brls::Image(get_resource_path() + "credits.png"));
-
     brls::Image* credits = new brls::Image(get_resource_path() + "credits.png");
     credits->setScaleType(brls::ImageScaleType::NO_RESIZE);
     appView->addTab("Introduction", credits);
@@ -354,8 +352,6 @@ void show_first_time_panel()
     list_about->setSpacing(20);
 
     appView->addTab("About this App", list_about);
-
-    //appView->sidebar->getChild(0)->setActionAvailable(brls::Key::A, false);
 
     brls::List* list_features1 = new brls::List();
     list_features1->addView(new brls::Header("NRO Features"));
@@ -415,16 +411,18 @@ void show_first_time_panel()
     brls::PopupFrame::open("Welcome To Homebrew Details", appView, "First Time Overview", "");
 }
 
-void show_whatsnew_panel()
+brls::List* generate_changelog_panel(std::string title, std::string str)
 {
-    brls::TabFrame* appView = new brls::TabFrame();
-    appView->sidebar->setWidth(250);
     brls::List* list = new brls::List();
 
-    std::vector<std::string> lines = explode("Long time, no see! For the last four months or so I've been rewriting the application almost completely from the ground up. Here's what's new.**Features:**- Scan files more than twice as fast*- Cache app lists to json and instantly reload them on next run without having to rescan*- Cache app icons for instantly reloading menus*- Sort list by categories (name, path, author, size, category)*- Sort list ascending or descending*- Group sorted lists by category (none, author, category, from appstore)*- Groundwork for language localization*- Groundwork for multiple view styles (icon grid and icon list coming soon)*- Welcome Page and Introduction for first run*- Settings is now a categorized json*- Pin Favorite apps to the top of the list*- Move Settings and Updater into their own panels accessible by list or main screen*- Delete folder if deleting app and it's the only thing in the folder*- Basically every other aspect rewritten for speed and stability**Frameworks:**- Updated nlohmann json*- Updated libnx*- Updated borealis ui*- Updated curl", '*');
+    brls::Label* title_label = new brls::Label(brls::LabelStyle::REGULAR, title);
+    title_label->setFontSize(30);
+    list->addView(title_label);
+
+    std::vector<std::string> lines = explode(str, '*');
     for (unsigned int i = 0; i < lines.size(); i++)
     {
-        unsigned int width = 47;
+        unsigned int width = 44;
 
         if (lines.at(i).size() != 0)
         {
@@ -442,8 +440,9 @@ void show_whatsnew_panel()
                 }
 
                 brls::ListItem* skinny2 = new brls::ListItem(line);
-                skinny2->setWidth(35);
+
                 skinny2->setHeight(35);
+                skinny2->setDrawTopSeparator(false);
                 skinny2->setDrawBottomSeparator(false);
                 list->addView(skinny2);
             }
@@ -466,7 +465,21 @@ void show_whatsnew_panel()
         list->addView(skinny);
     }
 
-    appView->addTab("v1.0", list);
+    return list;
+}
 
-    brls::PopupFrame::open("Welcome To Homebrew Details", appView, "First Time Overview", "");
+void show_whatsnew_panel()
+{
+    brls::TabFrame* appView = new brls::TabFrame();
+    appView->setIcon(get_resource_path() + "icon.png");
+    appView->sidebar->setWidth(250);
+
+    appView->addTab("v1.01", generate_changelog_panel("New Forwarder App", "Features:**- New \"What's New\" panel on main screen that shows recent changelogs (like this!)*- New Forwarder .nsp installable to your home menu**Fixes:**- Tiny potential problem affecting early 1.0 updates fixed*- Fix situation where blacklists may not apply**New Forwarder Overview:**- Display versions of HB-D on your card*- Choose and run your choice manually*- Automatically run your choice without additional input*- Included in this update and can be found at \"sdmc:/config/homebrew_details_next/forwarder*/HomebrewDetailsForwarder_v2.nsp\""));
+    appView->addTab("v1.0 ", generate_changelog_panel("The Complete Overhaul", "Long time, no see! For the last four months or so I've been rewriting the application almost completely from the ground up. Here's what's new.**Features:**- Scan files more than twice as fast*- Cache app lists to json and instantly reload them on next run without having to rescan*- Cache app icons for instantly reloading menus*- Sort list by categories (name, path, author, size, category)*- Sort list ascending or descending*- Group sorted lists by category (none, author, category, from appstore)*- Groundwork for language localization*- Groundwork for multiple view styles (icon grid and icon list coming soon)*- Welcome Page and Introduction for first run*- Settings is now a categorized json*- Pin Favorite apps to the top of the list*- Move Settings and Updater into their own panels accessible by list or main screen*- Delete folder if deleting app and it's the only thing in the folder*- Basically every other aspect rewritten for speed and stability**Frameworks:**- Updated nlohmann json*- Updated libnx*- Updated borealis ui*- Updated curl"));
+    appView->addTab("v0.95", generate_changelog_panel("Exit-To Settings", "- Added a new setting of what to exit to (between hb-menu and hb-d)"));
+    appView->addTab("v0.94", generate_changelog_panel("New Multi-Forwarder", "- Added a new multi-forwarder nsp that you can install to your home menu so you can organize the actual hb-d.nro where you'd like also an info panel with this information in the toolbox tab"));
+    appView->addTab("v0.93", generate_changelog_panel("Favorite App Pinning, Blacklist Folders", "- Added a new panel in the settings menu where you can add blacklisted folders (they'll be skipped during search)*- Added the ability to pin your favorite apps to the top of the list for easy access"));
+    appView->addTab("v0.92", generate_changelog_panel("Search Folder Blacklister", "- Added a new panel in the settings menu where you can add blacklisted folders (they'll be skipped during search)*- [Debug Mode Feature] Pinning favorite apps is mostly working but not quite done. If you wanna try it, you can enable debug mode in the settings!"));
+
+    brls::PopupFrame::open("What's New?", appView, "Recent Changelogs", "");
 }
